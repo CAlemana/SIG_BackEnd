1498dfde76c8e41da5d92ef0fd4f85e2b37baa85
CAlemana <car.alem07@gmail.com>
2023-03-19 12:15:28 -0600

    add query client

--- src/queries/client_query.js
+++ src/queries/client_query.js
@@ -1,7 +1,7 @@
 const get = "SELECT cedula, name, lastname, age, gender, height, weight, phone, start_date, end_date, bmi FROM client";
 const getById = "SELECT cedula, name, lastname, age, gender, height, weight, phone, start_date, end_date, bmi FROM client WHERE cedula = $1";
 const checkIdExists = "SELECT cedula, name, lastname, age, gender, height, weight, phone, start_date, end_date, bmi FROM client WHERE cedula = $1";
-const add = "INSERT INTO client (cedula, name, lastname, age, gender, height, weight, phone, start_date, end_date, bmi) VALUES ($1, $2, $3, $4, $5, $6, $7, $8)";
+const add = "INSERT INTO client (cedula, name, lastname, age, gender, height, weight, phone, start_date, end_date, bmi) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11)";
 const remove = "DELETE FROM client WHERE cedula = $1";
 const update = "UPDATE client SET cedula=$1, name=$2, lastname=$3, age=$4, gender=$5, height=$6, weight=$7, phone=$8, start_date=$9, end_date=$10, bmi=$11 WHERE cedula = $12";
 

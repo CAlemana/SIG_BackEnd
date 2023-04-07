3c2ff8de56ea29fd216b283792f3a185fab1316a
CAlemana <car.alem07@gmail.com>
2023-04-07 15:32:50 -0600

    Corrección

--- src/queries/revenue_query.js
+++ src/queries/revenue_query.js
@@ -3,7 +3,7 @@ const getById = "SELECT id, value, kind_value, date FROM revenue WHERE id = $1";
 const checkIdExists = "SELECT id, value, kind_value, date FROM revenue WHERE id = $1";
 const add = "INSERT INTO revenue (value, kind_value, date) VALUES ($1, $2, $3)";
 const remove = "DELETE FROM revenue WHERE id = $1";
-const update = "UPDATE client SET value=$1, kind_value=$2, date=$3 WHERE id = $4";
+const update = "UPDATE revenue SET value=$1, kind_value=$2, date=$3 WHERE id = $4";
 
 module.exports = {
     get,
